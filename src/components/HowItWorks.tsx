785d94a3891174c64bfdb72eeeddb6fc0a26e83c
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-02 20:14:25 +0300

    Enhance How it works section

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,73 +1,77 @@
-import React from "react";
+import React from 'react'
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "./ui/card";
-import { FileSearch, CalendarClock, Hospital } from "lucide-react";
+} from './ui/card'
+import { FileSearch, CalendarClock, Hospital } from 'lucide-react'
+
+type HowItWorksItem = {
+  title: string
+  icon: React.ReactNode
+  description: string
+  bgColor: string
+}
+
+const howItWorks: HowItWorksItem[] = [
+  {
+    title: 'Search for Doctor or Hospital',
+    icon: <FileSearch color="#B95000" size={52} absoluteStrokeWidth={true} />,
+    description:
+      'Find the right doctor or hospital by filtering through speciality, location or name',
+    bgColor: 'bg-[#FEF7E0]',
+  },
+  {
+    title: 'Book an Appointment',
+    icon: (
+      <CalendarClock color="#C5221F" size={52} absoluteStrokeWidth={true} />
+    ),
+    description:
+      'Schedule an appointment—whether online or in-person. Get instant confirmations and timely reminders.',
+    bgColor: 'bg-[#FEF0ED]',
+  },
+  {
+    title: 'Attend Your Appointment',
+    icon: <Hospital color="#137333" size={52} absoluteStrokeWidth={true} />,
+    description:
+      'Meet your doctor either virtually through a video call or in person at a hospital or clinic',
+    bgColor: 'bg-[#E6F4EA]',
+  },
+]
+
+const HowItWorksCard = ({ item }: { item: HowItWorksItem }) => (
+  <Card className="flex h-full max-h-80 flex-col items-center justify-center gap-6 border p-6">
+    <CardHeader>
+      <CardTitle
+        className={`flex size-20 items-center justify-center ${item.bgColor}`}
+      >
+        {item.icon}
+      </CardTitle>
+      <CardDescription className="text-lg font-medium text-primary">
+        {item.title}
+      </CardDescription>
+    </CardHeader>
+    <CardContent className="text-center text-muted-foreground">
+      {item.description}
+    </CardContent>
+  </Card>
+)
 
 export default function HowItWorks() {
   return (
     <section
       id="how-it-works"
-      className="flex flex-col gap-10 bg-popover px-16 pb-6 pt-24"
+      className="mx-auto flex flex-col items-center gap-8 pb-16 pt-64 sm:pt-44 lg:pt-36"
     >
-      <h2 className="text-center text-3xl text-primary">How It works</h2>
-      <div className="flex items-center justify-center gap-10">
-        <Card className="flex h-80 w-[410px] flex-col items-center justify-center gap-6">
-          <CardHeader>
-            <CardTitle className="h-20 w-20 items-center justify-center bg-[#FEF7E0]">
-              <FileSearch
-                color="#B95000"
-                size={52}
-                absoluteStrokeWidth={true}
-              />
-            </CardTitle>
-            <CardDescription>Search for Doctor or Hospital</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Find the right doctor or hospital by filtering through speciality,
-              location or name
-            </p>
-          </CardContent>
-        </Card>
+      <h2 className="section-title">How It works</h2>
 
-        <Card className="flex h-80 w-[410px] flex-col items-center justify-center gap-6">
-          <CardHeader>
-            <CardTitle className="h-20 w-20 items-center justify-center bg-[#FEF0ED]">
-              <CalendarClock
-                color="#C5221F"
-                size={52}
-                absoluteStrokeWidth={true}
-              />
-            </CardTitle>
-            <CardDescription>Book an Appointment</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Schedule an appointment—whether online or in-person. Get instant
-              confirmations and timely reminders.
-            </p>
-          </CardContent>
-        </Card>
-        <Card className="flex h-80 w-[410px] flex-col items-center justify-center gap-6">
-          <CardHeader>
-            <CardTitle className="h-20 w-20 items-center justify-center bg-[#E6F4EA]">
-              <Hospital color="#137333" size={52} absoluteStrokeWidth={true} />
-            </CardTitle>
-            <CardDescription>Attend Your Appointment</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Meet your doctor either virtually through a video call or in
-              person at a hospital or clinic
-            </p>
-          </CardContent>
-        </Card>
+      <div className="grid grid-rows-3 gap-6 md:grid-cols-3 md:grid-rows-1">
+        {howItWorks.map((item, index) => (
+          <HowItWorksCard key={index} item={item} />
+        ))}
       </div>
     </section>
-  );
+  )
 }
