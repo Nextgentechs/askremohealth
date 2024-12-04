be7590a91ea3caa1887bebd656fb40b725c5a169
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-04 08:23:17 +0300

    Improve responsiveness

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -104,8 +104,8 @@ export function SearchForm() {
 export default function HeroSection() {
   return (
     <section className="mt-12 flex h-40 w-full gap-8 px-4 sm:h-56 sm:flex-row sm:justify-between sm:gap-12 lg:gap-16 lg:px-12 xl:mt-0 xl:h-[432px]">
-      <div className="flex flex-grow flex-col items-center justify-center text-center sm:items-start sm:text-left">
-        <h1 className="text-2xl font-extrabold leading-tight text-primary transition-all duration-300 sm:text-3xl lg:text-3xl xl:text-5xl">
+      <div className="flex flex-grow flex-col items-center justify-center text-center lg:items-start lg:text-left">
+        <h1 className="text-2xl font-extrabold leading-tight text-primary transition-all duration-300 sm:text-3xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
           <div className="whitespace-nowrap">Solutions that help you and</div>
           <div className="whitespace-nowrap">your loved ones enjoy</div>
           <span className="whitespace-nowrap">
@@ -127,7 +127,7 @@ export default function HeroSection() {
       <Image
         src={Doctor}
         alt="Doctor Image"
-        className="hidden xl:block xl:h-[528px] xl:w-[520px] 2xl:h-[528px]"
+        className="hidden lg:block lg:h-[434px] w-[454px] xl:h-[528px] xl:w-[520px] 2xl:h-[528px]"
         priority
       />
     </section>
