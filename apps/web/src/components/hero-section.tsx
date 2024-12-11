df4ba6a370b43a4cb373f8376ad8bf2c1df62048
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-11 16:45:26 +0300

    Migrate to monorepo setup

--- apps/web/src/components/hero-section.tsx
+++ apps/web/src/components/hero-section.tsx
@@ -105,7 +105,7 @@ export default function HeroSection() {
   return (
     <section className="mt-6 flex h-40 w-full gap-8 px-4 sm:h-56 sm:flex-row sm:justify-between sm:gap-12 lg:mt-0 lg:h-[360px] lg:gap-16 lg:px-12 xl:mt-0 xl:h-[432px]">
       <div className="flex flex-grow flex-col items-center justify-center text-center lg:items-start lg:text-left">
-        <h1 className="text-2xl font-extrabold leading-tight text-primary transition-all duration-300 sm:text-3xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
+        <h1 className="text-2xl font-extrabold leading-tight text-primary transition-all duration-300 sm:text-3xl lg:text-4xl xl:text-5xl">
           <div className="whitespace-nowrap">Solutions that help you and</div>
           <div className="whitespace-nowrap">your loved ones enjoy</div>
           <span className="whitespace-nowrap">
