77ce6122251c745c47c4fe4fd1ad3d755c2fc3b1
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-12 15:38:46 +0300

    setup shadcn and customise font

--- apps/doctor/tailwind.config.ts
+++ apps/doctor/tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        geist: ["Geist", "sans serif"],
       },
       borderRadius: {
         lg: "var(--radius)",
