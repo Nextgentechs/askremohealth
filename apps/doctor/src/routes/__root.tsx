7c7c12bbcb4c35689d2975a5e9c820dddd7fed1c
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-12 20:46:28 +0300

    add UI components and form handling for login functionality

--- apps/doctor/src/routes/__root.tsx
+++ apps/doctor/src/routes/__root.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react'
-import { GeistSans } from 'geist/font/sans'
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
--- apps/doctor/src/routes/index.tsx
+++ apps/doctor/src/routes/index.tsx
@@ -1,17 +1,138 @@
-import * as React from "react";
-import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
+import logo from '@/assets/logo.svg'
+import { Moon } from 'lucide-react'
+import { z } from 'zod'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 
-export const Route = createFileRoute("/")({
+export const Route = createFileRoute('/')({
   component: HomeComponent,
-});
+})
 
 function HomeComponent() {
   return (
-    <div className=" h-full w-full m-auto">
-      <Button onClick={() => console.log("clicked")} className="font-normal">
-        Book Appointment
+    <div className="container flex min-h-dvh flex-col">
+      <Navbar />
+      <LoginCard />
+    </div>
+  )
+}
+
+function Navbar() {
+  return (
+    <div className="inline-flex h-fit w-full justify-between px-8 pt-5">
+      <img src={logo} />
+      <Button variant={'outline'} size={'icon'} className="rounded-full shadow">
+        <Moon />
       </Button>
     </div>
-  );
+  )
+}
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
+
+function LoginForm() {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <div className="flex items-center">
+                <FormLabel>Password</FormLabel>
+                <Link to="/" className="ml-auto inline-block text-sm underline">
+                  Forgot your password ?
+                </Link>
+              </div>
+
+              <FormControl>
+                <Input id="password" type="password" required {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </form>
+    </Form>
+  )
+}
+
+function LoginCard() {
+  return (
+    <Card className="m-auto w-full max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <LoginForm />
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{' '}
+          <Link href="#" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  )
 }
