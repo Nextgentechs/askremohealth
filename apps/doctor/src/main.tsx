82c04b3abc13a0d75479e486b730bbe40edf28ac
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-16 20:42:06 +0300

    create trpc-client in doctor's app

--- apps/doctor/src/main.tsx
+++ apps/doctor/src/main.tsx
@@ -1,26 +1,23 @@
-import React from "react";
-import "./styles/globals.css";
-import "unfonts.css";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
+import React from 'react'
+import './styles/globals.css'
+import 'unfonts.css'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
+import { createRouter } from './lib/trpc'
 
 // Set up a Router instance
-const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
-});
+const router = createRouter()
 
 // Register things for typesafety
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router;
+    router: typeof router
   }
 }
 
-const rootElement = document.getElementById("app")!;
+const rootElement = document.getElementById('app')!
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<RouterProvider router={router} />);
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(<RouterProvider router={router} />)
 }
