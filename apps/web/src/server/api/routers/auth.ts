55afeb96c522c2244162ba0091afba398a192c10
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-17 22:09:13 +0300

    add doctor's signup procedure

--- apps/web/src/server/api/routers/auth.ts
+++ apps/web/src/server/api/routers/auth.ts
@@ -1,9 +1,15 @@
 import { z } from 'zod'
 import { createTRPCRouter, publicProcedure } from '../trpc'
-import { certificates, doctors, users } from '@web/server/db/schema'
+import {
+  certificates,
+  doctors,
+  operatingHours,
+  users,
+} from '@web/server/db/schema'
 import bcrypt from 'bcrypt'
 import { TRPCError } from '@trpc/server'
 import { put } from '@vercel/blob'
+import { eq } from 'drizzle-orm'
 
 const operatingHoursSchema = z.object({
   day: z.enum([
@@ -15,36 +21,46 @@ const operatingHoursSchema = z.object({
     'Saturday',
     'Sunday',
   ]),
-  from: z.string(),
-  to: z.string(),
+  opening: z.string(),
+  closing: z.string(),
   isOpen: z.boolean(),
+})
+
+const signupSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email().optional(),
+  phone: z.string(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+  bio: z.string().optional(),
+  dob: z.string(),
+  specialty: z.string(),
+  subSpecialty: z.array(z.string()),
+  experience: z.string().transform((val) => parseInt(val)),
+  facility: z.string(),
+  registrationNumber: z.string(),
+  appointmentDuration: z.string(),
+  operatingHours: z.array(operatingHoursSchema),
+  medicalLicense: z.string().optional(),
 })
 
 export const authRouter = createTRPCRouter({
   /**Doctor's Auth */
 
   signup: publicProcedure
-    .input(
-      z.object({
-        firstName: z.string(),
-        lastName: z.string(),
-        email: z.string().email().optional(),
-        phone: z.string(),
-        password: z.string().min(8),
-        confirmPassword: z.string().min(8),
-        bio: z.string().optional(),
-        dob: z.string(),
-        specialty: z.string(),
-        subSpecialty: z.array(z.object({ id: z.string(), name: z.string() })),
-        experience: z.string().transform((val) => parseInt(val)),
-        facility: z.string(),
-        registrionNumber: z.string(),
-        appointmentDuration: z.string(),
-        operatingHours: z.array(operatingHoursSchema),
-        medicalLicense: z.string(),
-      }),
-    )
+    .input(signupSchema)
     .mutation(async ({ ctx, input }) => {
+      const user = await ctx.db.query.users.findFirst({
+        where: (user) => eq(user.phone, input.phone),
+      })
+
+      if (user) {
+        throw new TRPCError({
+          code: 'CONFLICT',
+          message: 'User already exists',
+        })
+      }
       await ctx.db.transaction(async (trx) => {
         const hashedPassword = await bcrypt.hash(input.password, 10)
 
@@ -72,31 +88,38 @@ export const authRouter = createTRPCRouter({
           id: user.id,
           bio: input.bio,
           specialty: input.specialty,
-          subSpecialties: input.subSpecialty,
+          subSpecialties: input.subSpecialty.map((id) => ({ id })),
           experience: input.experience,
           facility: input.facility,
-          licenseNumber: input.registrionNumber,
+          licenseNumber: input.registrationNumber,
         })
 
-        const buffer = Buffer.from(
-          input.medicalLicense.replace(/^data:application\/pdf;base64,/, ''),
-          'base64',
-        )
-
-        const { url, pathname } = await put(
-          `documents/${user.id}/${'medical-license'}`,
-          buffer,
-          {
-            access: 'public',
-            contentType: `application/pdf`,
-          },
-        )
-
-        await trx.insert(certificates).values({
+        await trx.insert(operatingHours).values({
           doctorId: user.id,
-          name: pathname,
-          url,
+          schedule: input.operatingHours,
         })
+
+        if (input.medicalLicense) {
+          const buffer = Buffer.from(
+            input.medicalLicense.replace(/^data:application\/pdf;base64,/, ''),
+            'base64',
+          )
+
+          const { url, pathname } = await put(
+            `documents/${user.id}/${'medical-license'}`,
+            buffer,
+            {
+              access: 'public',
+              contentType: `application/pdf`,
+            },
+          )
+
+          await trx.insert(certificates).values({
+            doctorId: user.id,
+            name: pathname,
+            url,
+          })
+        }
       })
 
       return { success: true }
