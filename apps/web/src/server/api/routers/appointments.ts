83890da20ce78ee89c9481c60b4b95a45a983a45
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-19 23:50:48 +0300

    Implement physical ppointments page

--- apps/web/src/server/api/routers/appointments.ts
+++ apps/web/src/server/api/routers/appointments.ts
@@ -2,23 +2,6 @@ import { z } from 'zod'
 import { createTRPCRouter, publicProcedure } from '../trpc'
 import { and, eq, gte } from 'drizzle-orm'
 
-const appointmentsSchema = z.object({
-  doctorId: z.string(),
-  patientId: z.string().optional(),
-  type: z.enum(['physical', 'online']),
-  status: z
-    .enum([
-      'scheduled',
-      'pending',
-      'completed',
-      'cancelled',
-      'rescheduled',
-      'missed',
-      'in_progress',
-    ])
-    .optional(),
-})
-
 export const appointmentsRouter = createTRPCRouter({
   doctor: {
     upcomming: publicProcedure
@@ -30,6 +13,62 @@ export const appointmentsRouter = createTRPCRouter({
               ctx.user?.id ? eq(appontment.doctorId, ctx.user.id) : undefined,
               eq(appontment.type, input),
               gte(appontment.appointmentDate, new Date()),
+            ),
+          columns: {
+            id: true,
+            appointmentDate: true,
+            status: true,
+            notes: true,
+          },
+          with: {
+            patient: {
+              with: {
+                user: {
+                  columns: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    phone: true,
+                  },
+                },
+              },
+            },
+          },
+        })
+      }),
+
+    listAll: publicProcedure
+      .input(
+        z.object({
+          type: z.enum(['physical', 'online']),
+          patientId: z.string().optional(),
+          startDate: z.date(),
+          endDate: z.date(),
+          status: z
+            .enum([
+              'scheduled',
+              'pending',
+              'completed',
+              'cancelled',
+              'rescheduled',
+              'missed',
+              'in_progress',
+            ])
+            .optional(),
+        }),
+      )
+      .query(async ({ ctx, input }) => {
+        return await ctx.db.query.appointments.findMany({
+          where: (appontment) =>
+            and(
+              ctx.user?.id ? eq(appontment.doctorId, ctx.user.id) : undefined,
+              eq(appontment.type, input.type),
+              input.status ? eq(appontment.status, input.status) : undefined,
+              input.patientId
+                ? eq(appontment.patientId, input.patientId)
+                : undefined,
+              gte(appontment.appointmentDate, input.startDate),
+              gte(appontment.appointmentDate, input.endDate),
             ),
           columns: {
             id: true,
