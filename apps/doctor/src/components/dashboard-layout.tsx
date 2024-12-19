a44155dff6ea1b6addfa15584f43669d44b88218
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-19 13:25:48 +0300

    add upcoming appointments route and update dashboard navigation

--- apps/doctor/src/components/dashboard-layout.tsx
+++ apps/doctor/src/components/dashboard-layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { SidebarInset, SidebarProvider, SidebarTrigger } from './ui/sidebar'
 import { AppSidebar } from './app-sidebar'
 import { Outlet } from '@tanstack/react-router'
@@ -8,11 +8,27 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
+  BreadcrumbSeparator,
 } from './ui/breadcrumb'
 import { Button } from './ui/button'
 import { BellDot, Moon } from 'lucide-react'
+import { useRouterState } from '@tanstack/react-router'
 
 function DashboardHeader() {
+  const routerState = useRouterState()
+  const pathSegments = useMemo(() => {
+    return routerState.location.pathname.split('/').filter(Boolean)
+  }, [routerState.location.pathname])
+
+  const formattedPaths = useMemo(() => {
+    return pathSegments.map((segment) =>
+      segment
+        .split('-')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' '),
+    )
+  }, [pathSegments])
+
   return (
     <header className="flex h-16 shrink-0 flex-row items-center justify-between gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
@@ -20,9 +36,14 @@ function DashboardHeader() {
         <Separator orientation="vertical" className="mr-2 h-4" />
         <Breadcrumb>
           <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
+            {formattedPaths.map((path, index) => (
+              <div key={index} className="inline-flex items-center gap-1.5">
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">{path}</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+              </div>
+            ))}
           </BreadcrumbList>
         </Breadcrumb>
       </div>
