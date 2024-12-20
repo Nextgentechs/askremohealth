af8899e7c7896fca0e023f1ff11090f2bbe31a9b
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2024-12-20 17:59:26 +0300

    implememt authentication check on dashboard routes

--- apps/doctor/src/routes/dashboard.tsx
+++ apps/doctor/src/routes/dashboard.tsx
@@ -1,6 +1,15 @@
 import DashboardLayout from '@/components/dashboard-layout'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashboard')({
+  beforeLoad: async ({ context, location }) => {
+    const user = await context.trpcQueryUtils.users.currentUser.ensureData()
+    if (!user) {
+      throw redirect({
+        to: '/auth/login',
+        search: { redirect: location.href },
+      })
+    }
+  },
   component: DashboardLayout,
 })
