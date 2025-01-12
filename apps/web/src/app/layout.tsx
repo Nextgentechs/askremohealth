ebc41aabe10d7532cbf8b147f0f2ebd7a40ce412
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2025-01-12 21:59:22 +0300

    integre current user data into navigation bar and layout.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,21 +6,23 @@ import Provider from '@web/providers/provider'
 import Footer from '@web/components/footer'
 import NavigationBar from '@web/components/navigation-bar'
 import { Toaster } from '@web/components/ui/toaster'
+import { api } from '@web/trpc/server'
 
 export const metadata: Metadata = {
   title: 'Ask Virtual Healthcare',
   description: 'Ask Virtual Healthcare',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const user = await api.users.currentUser()
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground antialiased">
         <Provider>
           <div className="mx-auto max-w-[1440px] px-4 pt-2 transition-all duration-300 sm:px-6 sm:pt-4 lg:px-8 lg:pt-6 xl:px-16">
-            <NavigationBar />
+            <NavigationBar user={user} />
           </div>
           {children}
           <Footer />
