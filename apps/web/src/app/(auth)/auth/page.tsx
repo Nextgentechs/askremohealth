68e40563e62e523df2f91aa990a096f6c78d490f
Kennedy Odhiambo Odwar <x.johnkennedy.x@gmail.com>
2025-02-14 22:47:43 +0300

    Improve new auth page

--- apps/web/src/app/(auth)/auth/page.tsx
+++ apps/web/src/app/(auth)/auth/page.tsx
@@ -1,14 +1,11 @@
-import { LoginForm } from '@web/components/login-form'
+import AuthForm from '@web/components/auth-form'
 import Logo from '@web/components/logo'
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-      <div className="flex w-full max-w-md flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <Logo />
-        </a>
-        <LoginForm />
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <AuthForm />
       </div>
     </div>
   )
