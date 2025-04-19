320d93edd97585558470c04f8ce49ee513c0f0d2
Roy Mwangi <royimwangi@gmail.com>
2025-04-19 14:27:54 +0300

    Add Contact and FAQ Sections

---
+++ apps/web/src/app/(app)/faq/page.tsx
@@ -0,0 +1,18 @@
+import Footer from '@web/components/footer'
+import FAQSection from '@web/components/faq-section'
+import FaqHeroSection from '@web/components/faq-hero'
+
+
+const Faq = () => {
+  return (
+    <div>
+      <FaqHeroSection/>
+      <FAQSection/>
+      
+      
+      <Footer />
+    </div>
+  )
+}
+
+export default Faq
---
+++ apps/web/src/components/contact-hero.tsx
@@ -0,0 +1,14 @@
+
+
+export default function ContactHeroSection() {
+    return (
+      <section className="mt-4 w-full sm:h-56 items-center py-24 text-center lg:mt-0 lg:h-[160px] lg:gap-16 lg:px-12 xl:mt-0 xl:h-[232px] bg-gradient-to-b from-white to-secondary">
+        <h2 className="section-title md:text-4xl">How can you reach us?</h2>
+       
+  
+        
+        
+      </section>
+    )
+  }
+  
