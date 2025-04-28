6d3ea7516f142a5b9f0b4ae1176e97ee27b438cf
krisine <kristine.nyaga@student.moringaschool.com>
2025-04-28 21:05:44 +0300

    admin template v1

---
+++ apps/web/src/app/admin-template/layout.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import { AppSidebar } from '@web/components/admin/app-sidebar'
+import { Separator } from '@web/components/ui/separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@web/components/ui/sidebar'
+
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset className="flex flex-col gap-4">
+        <DashboardHeader />
+        <div className="mx-10">
+          <React.Suspense fallback={<ProgressBar />}>{children}</React.Suspense>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
+
+function ProgressBar() {
+  return (
+    <div className="mx-auto flex h-[90vh] w-screen max-w-md flex-row items-center justify-center">
+      <div className="relative h-1 w-full overflow-hidden bg-gray-300">
+        <div className="absolute left-0 top-0 h-full w-1/2 animate-streaming-progress bg-primary"></div>
+      </div>
+    </div>
+  )
+}
+
+function DashboardHeader() {
+  return (
+    <header className="flex h-16 shrink-0 flex-row items-center justify-between gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <div className="flex items-center gap-2 px-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+      </div>
+    </header>
+  )
+}
