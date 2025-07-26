e17fb53634b9669d9b0b2b345d9c7cc62514c8d2
Bill Albert <Bill.Albert@Admins-MacBook-Pro-8.local>
2025-07-27 00:54:16 +0300

    Kicks in change in contact hero section

--- apps/web/src/components/contact-hero.tsx
+++ apps/web/src/components/contact-hero.tsx
@@ -3,8 +3,8 @@
 export default function ContactHeroSection() {
     return (
       <section className="mt-4 w-full h-32 items-center py-12 text-center lg:mt-0 lg:gap-16 lg:px-12 xl:mt-0 xl:h-[162px] bg-gradient-to-b from-white to-secondary">
-        <h2 className="section-title md:text-4xl">How can you reach us?</h2>
-        <h2 className="section-title md:text-4xl">Call us?</h2>
+        <h2 className="section-title md:text-4xl">How can you reach TEST us? </h2>
+     
        
   
         
