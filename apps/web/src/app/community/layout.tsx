e14b890a209f9fd790fa907dcdcb983ff59c3cbe
Levin010 <levinomwenga10@gmail.com>
2025-09-19 12:53:26 +0000

    Refactor CommunityLayout to use CommunityNavbar

--- apps/web/src/app/community/layout.tsx
+++ apps/web/src/app/community/layout.tsx
@@ -1,5 +1,4 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import NavigationBar from '@web/components/navigation-bar'
+import CommunityNavbar from '@web/components/community/CommunityNavbar'
 
 
 export default function CommunityLayout({
@@ -8,15 +7,13 @@ export default function CommunityLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <div className="bg-gradient-to-b from-white to-orange-50 min-h-screen">
-        <div className="mx-auto max-w-[1440px] px-4 pt-2 pb-2 transition-all duration-300 sm:px-6 sm:pt-4 lg:px-8 lg:pt-6 xl:px-16">
-          <NavigationBar /> 
+    <div className="bg-gradient-to-b from-white to-orange-50 min-h-screen">
+        <div className="mx-auto max-w-[1440px] px-4 pt-2 pb-2 transition-all duration-300 sm:px-6 sm:pt-4 lg:px-8 lg:pt-6 xl:px-8">
+          <CommunityNavbar />
         </div>
         <div className="px-4 md:px-4 lg:px-4 xl:px-4 2xl:px-4">
           {children}
         </div>
-      </div>
-    </ClerkProvider>
+    </div>
   );
 }
