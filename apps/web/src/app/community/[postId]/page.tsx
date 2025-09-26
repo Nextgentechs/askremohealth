1cde6069e17b01baeeffefc5fa2856e50f55d2d1
Levin010 <levinomwenga10@gmail.com>
2025-09-26 13:48:48 +0000

    Add SinglePost and SinglePostPage components; implement post fetching and display logic

---
+++ apps/web/src/app/community/[postId]/page.tsx
@@ -0,0 +1,30 @@
+import LeftMenu from "@web/components/community/leftMenu/LeftMenu";
+import RightMenu from "@web/components/community/rightMenu/RightMenu";
+import SinglePost from "@web/components/community/feed/SinglePost";
+
+interface SinglePostPageProps {
+  params: {
+    postId: string;
+  };
+}
+
+const SinglePostPage = ({ params }: SinglePostPageProps) => {
+
+    const { postId } = params;
+
+  return (
+    <div className="flex gap-6 pt-1">
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu type="home" />
+      </div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <SinglePost postId={postId} />
+      </div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu />
+      </div>
+    </div>
+  );
+};
+
+export default SinglePostPage;
