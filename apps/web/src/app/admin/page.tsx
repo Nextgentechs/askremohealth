a8c10e4746b77a2555021aea9ff07ad2e519c6f2
BengoLamech <bengolamech3@gmail.com>
2025-10-11 20:52:35 +0300

    test page

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation'
 
 export default function AdminPage() {
   // Imperative redirect
-  return redirect('/admin/doctors')
+  return redirect('/admin/test')
 }
