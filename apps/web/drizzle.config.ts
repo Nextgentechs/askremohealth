fb3e91f8e6d62f386f720e1315178faa09dc2f46
BengoLamech <bengolamech3@gmail.com>
2025-10-22 12:46:04 +0300

    changes to database

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -6,7 +6,10 @@ export default {
   schema: './src/server/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.DATABASE_URL  + '?sslmode=require',
+    ssl: {
+      rejectUnauthorized: false, // Neon uses self-signed certs
+    },
   },
 } satisfies Config
 
