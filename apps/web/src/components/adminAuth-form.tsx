d4b4f0c4a92ef7153f574f237f5590b773826277
BengoLamech <bengolamech3@gmail.com>
2025-11-26 23:17:07 +0300

    trial 50

--- apps/web/src/components/adminAuth-form.tsx
+++ apps/web/src/components/adminAuth-form.tsx
@@ -616,7 +616,7 @@ function InputOTPForm({loggedInEmail}:{loggedInEmail:string}) {
     } else if (user.role === 'admin') {
       // navigate to admin subdomain path on the current origin
       // If you need to land on admin.askremohealth domain specifically:
-      window.location.href = 'https://admin.askremohealth/admin/doctors'
+      window.location.href = '/admin/doctors'
       //router.push('/admin/doctors')
     } else {
       router.push('/')
