e4ed20f3c352558b563817dcc8e0cedb44fabe40
BengoLamech <bengolamech3@gmail.com>
2025-11-27 17:25:25 +0300

    trial 111

--- apps/web/src/components/admin/app-sidebar.tsx
+++ apps/web/src/components/admin/app-sidebar.tsx
@@ -103,7 +103,7 @@ function NavUser() {
                 }}
               >
                 <LogOut className="mr-2 h-4 w-4" />
-                Log out
+                Sign-out
               </DropdownMenuItem>
             </DropdownMenuGroup>
           </DropdownMenuContent>
